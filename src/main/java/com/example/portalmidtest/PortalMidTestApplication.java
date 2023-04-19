d4719eb743f7f76cbeea01adcf12dbb4722abe36
tnrms08 <wjddlfla661@naver.com>
2023-04-19 23:33:07 +0900

    초기설정

---
+++ src/main/java/com/example/portalmidtest/PortalMidTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.portalmidtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalMidTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalMidTestApplication.class, args);
+    }
+
+}
