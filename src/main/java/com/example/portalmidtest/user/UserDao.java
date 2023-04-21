52ed741c3ee72421a758e41cf62ab57f7cda89b0
tnrms08 <wjddlfla661@naver.com>
2023-04-21 16:17:45 +0900

    2번

--- src/main/java/com/example/portalmidtest/user/UserDao.java
+++ src/main/java/com/example/portalmidtest/user/UserDao.java
@@ -2,10 +2,9 @@ package com.example.portalmidtest.user;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public User findById(Long id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
+        Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select id, name, password from userinfo where id = ?");
         preparedStatement.setLong(1, id);
         ResultSet resultSet = preparedStatement.executeQuery();
@@ -20,9 +19,12 @@ public class UserDao {
 
         return user;
     }
+
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+
+
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
+        Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo (name, password) values ( ?,? )",Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, user.getName());
         preparedStatement.setString(2,user.getPassword());
