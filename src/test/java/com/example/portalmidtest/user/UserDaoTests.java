6ba0cb47b4e2c0f783dfa551bd5601a6e9d6e7e0
tnrms08 <wjddlfla661@naver.com>
2023-04-21 16:25:20 +0900

    5번

--- src/test/java/com/example/portalmidtest/user/UserDaoTests.java
+++ src/test/java/com/example/portalmidtest/user/UserDaoTests.java
@@ -1,6 +1,9 @@
 package com.example.portalmidtest.user;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -9,13 +12,18 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
+    private static UserDao userDao;
+    @BeforeAll
+    public static void setUp(){
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao",UserDao.class);
+    }
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Long id = 1l;
         String name = "hulk";
         String password = "1234";
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
+
         User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -28,8 +36,7 @@ public class UserDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
+
         userDao.insert(user);
         assertThat(user.getId(),greaterThan(1l));
         User insertedUser = userDao.findById(user.getId());
