c97456886628a4be5e1bcfc77effbf913f96331e
tnrms08 <wjddlfla661@naver.com>
2023-04-21 16:28:59 +0900

    6번

--- src/main/java/com/example/portalmidtest/user/DaoFactory.java
+++ src/main/java/com/example/portalmidtest/user/DaoFactory.java
@@ -2,17 +2,32 @@ package com.example.portalmidtest.user;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
     @Bean
-    public UserDao userDao() {
-        UserDao userDao = new UserDao(connectionMaker());
+    public UserDao userDao() throws ClassNotFoundException {
+        UserDao userDao = new UserDao(dataSource());
         return userDao;
     }
 
+    private String username = "jeju";
+    private String password = "jejupw";
+    private String className = "com.mysql.cj.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost/jeju";
+
     @Bean
-    public ConnectionMaker connectionMaker() {
-        return new JejuConnectionMaker();
+    public DataSource dataSource() throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setUrl(url);
+        return dataSource;
     }
+
 }
--- src/main/java/com/example/portalmidtest/user/UserDao.java
+++ src/main/java/com/example/portalmidtest/user/UserDao.java
@@ -1,16 +1,17 @@
 package com.example.portalmidtest.user;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final ConnectionMaker connectionMaker;
+    private final DataSource dataSource;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public User findById(Long id) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionMaker.getConnection();
+        Connection connection = dataSource.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select id, name, password from userinfo where id = ?");
         preparedStatement.setLong(1, id);
         ResultSet resultSet = preparedStatement.executeQuery();
@@ -26,11 +27,8 @@ public class UserDao {
         return user;
     }
 
-
-
-
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionMaker.getConnection();
+        Connection connection = dataSource.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo (name, password) values ( ?,? )",Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, user.getName());
         preparedStatement.setString(2,user.getPassword());
