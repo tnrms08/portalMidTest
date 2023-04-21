bc69999b25e663a201294f466e15b1eda023cc47
tnrms08 <wjddlfla661@naver.com>
2023-04-21 16:11:06 +0900

    1번

--- src/test/java/com/example/portalmidtest/user/UserDaoTests.java
+++ src/test/java/com/example/portalmidtest/user/UserDaoTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
@@ -18,5 +19,20 @@ public class UserDaoTests {
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
+    }
+    @Test
+    public void insert() throws SQLException, ClassNotFoundException {
+        String name = "misuk";
+        String password = "1111";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+        assertThat(user.getId(),greaterThan(1l));
+        User insertedUser = userDao.findById(user.getId());
+        assertThat(insertedUser.getId(),is(user.getId()));
+        assertThat(insertedUser.getName(),is(user.getName()));
+        assertThat(insertedUser.getPassword(),is(user.getPassword()));
     }
 }
